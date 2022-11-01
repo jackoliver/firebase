595d035d0bb5be4fdb9a5c882f5a5f4d7bde742d
JackEdward Oliver <jack@jv-la.com>
2022-11-01 23:31:06 +0100

    All the mantine styling!!!11!!1

--- apps/exercise-1/src/routes/Login/Login.tsx
+++ apps/exercise-1/src/routes/Login/Login.tsx
@@ -1,10 +1,42 @@
+import { css } from '@emotion/react';
 import { FormEventHandler, useState } from 'react';
 import { useAuth } from '@fb/shared-auth';
 import { Link, Navigate } from 'react-router-dom';
+import { Input, Button, Box } from '@mantine/core';
 
 const INITIAL_VALUES = {
   email: '',
   password: '',
+};
+
+const STYLES = {
+  base: css`
+    background: #f3f3f3;
+    min-height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  box: css`
+    background: white;
+    border-radius: 2px;
+    padding: 20px;
+    width: 350px;
+    box-shadow: 0 5px 10px -5px rgba(0, 0, 0, 0.1);
+
+    > * {
+      margin-bottom: 8px;
+    }
+
+    a {
+      text-align: center;
+      display: block;
+      font-size: 0.9rem;
+      line-height: 1;
+      margin-top: 16px;
+    }
+  `,
 };
 
 export const Login = () => {
@@ -33,25 +65,29 @@ export const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        onChange={handleChange}
-        name="email"
-        value={values.email}
-        type="email"
-        placeholder="Email"
-        required
-      />
-      <input
-        onChange={handleChange}
-        name="password"
-        value={values.password}
-        type="password"
-        placeholder="Password"
-        required
-      />
-      <button>Log in</button>
-      <Link to="/signup">Create an account</Link>
+    <form css={STYLES.base} onSubmit={handleSubmit}>
+      <Box css={STYLES.box}>
+        <Input
+          onChange={handleChange}
+          name="email"
+          value={values.email}
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <Input
+          onChange={handleChange}
+          name="password"
+          value={values.password}
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <Button type="submit" fullWidth>
+          Log in
+        </Button>
+        <Link to="/signup">Create an account</Link>
+      </Box>
     </form>
   );
 };
--- apps/exercise-1/src/routes/Signup/Signup.tsx
+++ apps/exercise-1/src/routes/Signup/Signup.tsx
@@ -1,12 +1,44 @@
+import { css } from '@emotion/react';
 import { FormEventHandler, useState } from 'react';
 import { useAuth } from '@fb/shared-auth';
 import { Link, Navigate } from 'react-router-dom';
+import { Input, Button, Box } from '@mantine/core';
 
 const INITIAL_VALUES = {
   firstName: '',
   lastName: '',
   email: '',
   password: '',
+};
+
+const STYLES = {
+  base: css`
+    background: #f3f3f3;
+    min-height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  box: css`
+    background: white;
+    border-radius: 2px;
+    padding: 20px;
+    width: 350px;
+    box-shadow: 0 5px 10px -5px rgba(0, 0, 0, 0.1);
+
+    > * {
+      margin-bottom: 8px;
+    }
+
+    a {
+      text-align: center;
+      display: block;
+      font-size: 0.9rem;
+      line-height: 1;
+      margin-top: 16px;
+    }
+  `,
 };
 
 export const Signup = () => {
@@ -35,37 +67,41 @@ export const Signup = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        onChange={handleChange}
-        name="firstName"
-        value={values.firstName}
-        type="text"
-        placeholder="First name"
-      />
-      <input
-        onChange={handleChange}
-        name="lastName"
-        value={values.lastName}
-        type="text"
-        placeholder="Last name"
-      />
-      <input
-        onChange={handleChange}
-        name="email"
-        value={values.email}
-        type="email"
-        placeholder="Email"
-      />
-      <input
-        onChange={handleChange}
-        name="password"
-        value={values.password}
-        type="password"
-        placeholder="Password"
-      />
-      <button>Sign up</button>
-      <Link to="/login">Already have an account? Log in</Link>
+    <form css={STYLES.base} onSubmit={handleSubmit}>
+      <Box css={STYLES.box}>
+        <Input
+          onChange={handleChange}
+          name="firstName"
+          value={values.firstName}
+          type="text"
+          placeholder="First name"
+        />
+        <Input
+          onChange={handleChange}
+          name="lastName"
+          value={values.lastName}
+          type="text"
+          placeholder="Last name"
+        />
+        <Input
+          onChange={handleChange}
+          name="email"
+          value={values.email}
+          type="email"
+          placeholder="Email"
+        />
+        <Input
+          onChange={handleChange}
+          name="password"
+          value={values.password}
+          type="password"
+          placeholder="Password"
+        />
+        <Button type="submit" fullWidth>
+          Sign up
+        </Button>
+        <Link to="/login">Already have an account? Log in</Link>
+      </Box>
     </form>
   );
 };
