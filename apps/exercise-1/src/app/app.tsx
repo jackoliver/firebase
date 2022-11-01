2693909bad0ba18c97ba34341c8c9df453259c8e
JackEdward Oliver <jack@jv-la.com>
2022-11-01 22:49:20 +0100

    Scaffy form

--- apps/exercise-1/src/app/app.tsx
+++ apps/exercise-1/src/app/app.tsx
@@ -1,5 +1,15 @@
+import { FormEventHandler } from 'react';
 export function App() {
-  return <div>This is the application.</div>;
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Sign up</button>
+    </form>
+  );
 }
 
 export default App;
