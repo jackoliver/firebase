22ea95e6f00ca23fdadc5f176b6d92322079360d
JackEdward Oliver <jack@jv-la.com>
2022-11-02 02:48:58 +0100

    Refactor styles ChatWindow

---
+++ apps/exercise-2/src/components/Chat/ChatWindow/ChatWindow.styles.ts
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react';
+const styles = css`
+  background: #f3f3f3;
+  flex: 1 0 auto;
+  padding: 16px;
+  height: calc(100vh - 128px);
+  overflow-y: auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export default styles;
--- apps/exercise-2/src/components/Chat/ChatWindow/ChatWindow.tsx
+++ apps/exercise-2/src/components/Chat/ChatWindow/ChatWindow.tsx
@@ -12,6 +12,7 @@ import {
 
 import { ChatMessage } from '../ChatMessage';
 import { ZeroMessages } from 'components';
+import styles from './ChatWindow.styles';
 
 interface IMessageItem {
   id: string;
@@ -21,18 +22,6 @@ interface IMessageItem {
 }
 
 export const ChatWindow = () => {
-  const STYLES = css`
-    background: #f3f3f3;
-    flex: 1 0 auto;
-    padding: 16px;
-    height: calc(100vh - 128px);
-    overflow-y: auto;
-
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-  `;
-
   // State variable to store messages (set in useEffect)
   const [messagesData, setMessagesData] = useState<DocumentData | []>([]);
 
@@ -73,7 +62,7 @@ export const ChatWindow = () => {
   const hasMessages = messagesData && messagesData['length'] > 0;
 
   return (
-    <div id="chat" css={STYLES}>
+    <div id="chat" css={styles}>
       {/* This is where we display messages */}
       {hasMessages ? (
         messagesData.map((messageItem: IMessageItem) => (
