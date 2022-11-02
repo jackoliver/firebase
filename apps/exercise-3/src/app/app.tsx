178518a127dd34c8f432b46e62377520109a6216
JackEdward Oliver <jack@jv-la.com>
2022-11-02 03:10:29 +0100

    Yes all the things please

--- apps/exercise-3/src/app/app.tsx
+++ apps/exercise-3/src/app/app.tsx
@@ -1,15 +1,46 @@
-import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import { css } from '@emotion/react';
 
 export function App() {
+  const styles = css`
+    background: coral;
+    font-family: 'Inter', sans-serif;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    .content {
+      position: relative;
+      background: white;
+      padding: 24px;
+
+      strong {
+        font-weight: 700;
+      }
+    }
+  `;
   return (
-    <StyledApp>
-      <NxWelcome title="exercise-3" />
-    </StyledApp>
+    <div css={styles}>
+      <img
+        src="https://media.giphy.com/media/26u4cqiYI30juCOGY/giphy.gif"
+        alt=""
+      />
+      <div className="content">
+        <strong>Congratulations!</strong> You just deployed an entire freaking
+        website.
+      </div>
+    </div>
   );
 }
 
--- apps/exercise-3/src/main.tsx
+++ apps/exercise-3/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Global } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 
 import App from './app/app';
 
@@ -8,6 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
+    <Global styles={emotionReset} />
     <App />
   </StrictMode>
 );
